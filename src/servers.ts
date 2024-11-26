7fb90b0444f0f154cde4b38559253b1a8b493d82
Viktor Havaka <viktor.govako@gmail.com>
2024-11-26 20:25:54 -0300

    Enable iOS donates for TestFlight (241126).
    Signed-off-by: Viktor Havaka <viktor.govako@gmail.com>

--- src/servers.ts
+++ src/servers.ts
@@ -184,7 +184,7 @@ export async function getServersList(request: Request) {
 
   // Disable donates for reviewers for all app versions AFTER this one.
   const lastApprovedAndReleasedGoogleAppVersionCode = 241112;
-  const lastApprovedAndReleasediOSAppVersionCode = 241112;
+  const lastApprovedAndReleasediOSAppVersionCode = 241126;
   let donatesEnabled = true;
   if (
     appVersion.flavor === 'google' &&
