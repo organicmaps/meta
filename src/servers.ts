172495626de6610bcc923269fcd18f4aa6e61559
Viktor Havaka <viktor.govako@gmail.com>
2025-07-14 15:05:10 -0300

    Enable donates for Google Play.
    Signed-off-by: Viktor Havaka <viktor.govako@gmail.com>

--- src/servers.ts
+++ src/servers.ts
@@ -213,7 +213,7 @@ export async function getServersList(request: Request) {
   };
 
   // Disable donates for reviewers for all app versions AFTER this one.
-  const lastApprovedAndReleasedGoogleAppVersionCode = 250613;
+  const lastApprovedAndReleasedGoogleAppVersionCode = 250713;
   const lastApprovedAndReleasediOSAppVersionCode = 250713;
   let donatesEnabled = true;
   if (
