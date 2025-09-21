780438734966849a973e86f82e98d1cca815f020
Viktor Havaka <viktor.govako@gmail.com>
2025-09-21 08:48:14 -0300

    Enable donates for AppStore.
    Signed-off-by: Viktor Havaka <viktor.govako@gmail.com>

--- src/servers.ts
+++ src/servers.ts
@@ -218,7 +218,7 @@ export async function getServersList(request: Request) {
 
   // Disable donates for reviewers for all app versions AFTER this one.
   const lastApprovedAndReleasedGoogleAppVersionCode = 250916;
-  const lastApprovedAndReleasediOSAppVersionCode = 250916;
+  const lastApprovedAndReleasediOSAppVersionCode = 250920;
   let donatesEnabled = true;
   if (
     appVersion.flavor === 'google' &&
